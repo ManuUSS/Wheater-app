53ae069483d852928bd0ede9fe839505ed0eb525
Manuel Ulate <fr3zs09@gmail.com>
2023-02-22 21:39:31 -0600

    About removed

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,11 +6,6 @@ const routes: Array<RouteRecordRaw> = [
     path: '/',
     name: 'home',
     component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
 
