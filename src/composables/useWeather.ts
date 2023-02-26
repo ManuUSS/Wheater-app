b664a3944c76c0570a6b77168d61e0304cdaede4
Manuel Ulate <fr3zs09@gmail.com>
2023-02-25 21:00:54 -0600

    generate random weathers and cities around the world helper

--- src/composables/useWeather.ts
+++ src/composables/useWeather.ts
@@ -2,6 +2,7 @@ import { WeatherResponse } from '@/interfaces/weather';
 import { destructurePromises } from '@/utils';
 import { ref } from 'vue';
 import { weatherApi } from '../api/weatherApi';
+import { citiesAroundTheWorld } from '../helpers/regions';
 
 
 export const useWeather = () => {
@@ -10,55 +11,64 @@ export const useWeather = () => {
 
     const getWeather = async () => {
         
-        const parisWeather = weatherApi.get<WeatherResponse>('/current.json', {
+        const num1 = Math.floor( Math.random() * 100 );
+        const num2 = Math.floor( Math.random() * 100 );
+        const num3 = Math.floor( Math.random() * 100 );
+        const num4 = Math.floor( Math.random() * 100 );
+
+        const city1 = citiesAroundTheWorld[ num1 ].toLowerCase();
+        const city2 = citiesAroundTheWorld[ num2 ].toLowerCase();
+        const city3 = citiesAroundTheWorld[ num3 ].toLowerCase();
+        const city4 = citiesAroundTheWorld[ num4 ].toLowerCase();
+
+        const weather1 = weatherApi.get<WeatherResponse>('/current.json', {
             params: {
-                q: 'Paris'
+                q: city1
             }
         });
         
-        const newYorkWeather = weatherApi.get<WeatherResponse>('/current.json', {
+        const weather2 = weatherApi.get<WeatherResponse>('/current.json', {
             params: {
-                q: 'New York'
+                q: city2
             }
         });
         
-        const sanJoseWeather = weatherApi.get<WeatherResponse>('/current.json', {
+        const weather3 = weatherApi.get<WeatherResponse>('/current.json', {
             params: {
-                q: 'Costa Rica'
+                q: city3
             }
         });
         
-        const rioJaneiroWeather = weatherApi.get<WeatherResponse>('/current.json', {
+        const weather4 = weatherApi.get<WeatherResponse>('/current.json', {
             params: {
-                q: 'Rio Janeiro'
+                q: city4
             }
         });
 
         const [ 
-            weatherParis, 
-            weatherNewYork, 
-            weatherSanJose, 
-            weatherRioJaneiro  
+            weatherPromise1, 
+            weatherPromise2, 
+            weatherPromise3, 
+            weatherPromise4  
         ] = await Promise.all(
             [ 
-                parisWeather, 
-                newYorkWeather, 
-                sanJoseWeather, 
-                rioJaneiroWeather 
+                weather1, 
+                weather2, 
+                weather3, 
+                weather4 
             ]);
             
-        const weatherParisResponse      =  destructurePromises( weatherParis ); 
-        const weatherNewYorkResponse    =  destructurePromises( weatherNewYork ); 
-        const weatherSanJoseResponse    =  destructurePromises( weatherSanJose ); 
-        const weatherRioJaneiroResponse =  destructurePromises( weatherRioJaneiro ); 
+        const weatherResponse1 =  destructurePromises( weatherPromise1 ); 
+        const weatherResponse2 =  destructurePromises( weatherPromise2 ); 
+        const weatherResponse3 =  destructurePromises( weatherPromise3 ); 
+        const weatherResponse4 =  destructurePromises( weatherPromise4 ); 
 
         defaultWeathers.value = [ 
-            weatherParisResponse, 
-            weatherNewYorkResponse, 
-            weatherSanJoseResponse, 
-            weatherRioJaneiroResponse 
+            weatherResponse1, 
+            weatherResponse2, 
+            weatherResponse3, 
+            weatherResponse4 
         ];
-        console.log(defaultWeathers.value);
     }
 
     getWeather();
