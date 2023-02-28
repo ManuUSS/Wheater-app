3bf03347b3b24b92dd4150a2e26b990c6a2f2cbb
Manuel Ulate <fr3zs09@gmail.com>
2023-02-28 15:38:11 -0600

    styling searcher and submit button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,15 +4,18 @@
       <h1>Manu's Weather App</h1>
     </div>
     <div class="weather-body">
-      <form action="">
+      <form>
         <input 
           class="input-weather"
           type="text" 
           placeholder="Search a zone"
         />
-        <input 
-          type="submit" 
-        />
+        <button 
+          class="input-weather_submit" 
+          type="submit"
+        >
+          Search
+        </button>
       </form>
       <div 
         class="wrapper-cards"
@@ -111,8 +114,39 @@ export default defineComponent({
     flex-grow: 1;
   }
   
-  .input-weather {
-    
+  form {
+    border: none;
+    padding: 0;
+    display: flex;
+    gap: 0.5rem;
   }
+
+  .input-weather {
+    width: 260px;
+    border: 1px solid #555;
+    border-radius: 1.375rem;
+    display: block;
+    padding: 9px 4px 9px 40px;
+    background: #FFFFFF url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' class='bi bi-search' viewBox='0 0 16 16'%3E%3Cpath d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z'%3E%3C/path%3E%3C/svg%3E") no-repeat 13px center;
+  }
+
+  .input-weather_submit {
+    border-radius: 0.9375rem;
+    padding: 0.5rem;
+    background: transparent;
+    color: #FFFFFF;
+    border: 1px solid #FFFFFF;
+  }
+
+  .input-weather_submit:hover {
+    opacity: 1;
+  }
+
+  .input-weather:focus,
+  .input-weather_submit:focus {
+    box-shadow: 0 0 3px 0 #1183d6;
+    border-color: #1183d6;
+    outline: none;
+}
 
 </style>
