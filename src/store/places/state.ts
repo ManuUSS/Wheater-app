05f7dd8373f5c6589abe4a5659c0798bb7507e36
Manuel Ulate <fr3zs09@gmail.com>
2023-03-02 20:49:55 -0600

    state inital state

--- src/store/places/state.ts
+++ src/store/places/state.ts
@@ -1,10 +1,33 @@
-export interface ExampleStateInterface {
-    prop: boolean;
+import { WeatherResponse } from '../../interfaces/weather';
+export interface PlacesWeatherState {
+    isLoading: boolean;
+    placeWeather: WeatherResponse;
 }
 
-function state(): ExampleStateInterface {
+function state(): PlacesWeatherState {
     return {
-        prop: true,
+        isLoading: true,
+        placeWeather: {
+            location: {
+                name: '',
+                region: '',
+                country: ''
+            },
+            current: {
+                temp_c: 0,
+                temp_f: 0,
+                is_day: 0,
+                condition: {
+                    text: '',
+                    icon: '',
+                    code: 0
+                },
+                wind_kph: 0,
+                wind_dir: '',
+                precip_mm: 0,
+                humidity: 0,
+            }
+        }
     }
 }
 
